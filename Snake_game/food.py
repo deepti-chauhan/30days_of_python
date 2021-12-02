28ca6f9d87971a2b67c1a8d71b623f801ae23684
Deepti Chauhan <76423762+deepti-chauhan@users.noreply.github.com>
2021-12-02 23:54:36 +0530

    Update food.py

--- Snake_game/food.py
+++ Snake_game/food.py
@@ -9,6 +9,9 @@ class food(Turtle):
 		self.color("yellow")
 		self.shapesize(stretch_len = 0.5, stretch_wid=0.5)
 		self.speed("fastest")
+		self.refresh()
+		
+	def refresh(self):
 		rand_x = random.randint(-280,280)
 		rand_y = random.randint(-280,280)
 		self.goto(rand_x,rand_y)
