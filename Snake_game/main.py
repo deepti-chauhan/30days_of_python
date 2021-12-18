7dce761d06e47854aa4c99ac464f4bcab2b459e9
Deepti Chauhan <76423762+deepti-chauhan@users.noreply.github.com>
2021-12-18 21:46:48 +0530

    Update main.py

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -26,21 +26,22 @@ while game_is_on:
 	time.sleep(0.1)
 	snake.move()
 
+	#detection with food
 	if snake.head.distance(food) <15:
 		food.refresh()
 		snake.extend()
 		score.increase_score()
 
+	#detection with wall	
 	if snake.head.xcor() < -290 or snake.head.xcor() > 290 or snake.head.ycor() < -290 or snake.head.ycor() > 290:
-		game_is_on = False
-		score.game_over()
+		score.reset_score()
+		snake.reset_snake()
 
 	#tail collision detection
 	for segment in snake.segments:
 		if segment == snake.head:
 			pass
-		elif snake.head.distance(segment)	< 10:
-			game_is_on = False
-			score.game_over()
+		elif snake.head.distance(segment) < 10:
+			score.reset_score()
 
 screen.exitonclick()
